a1a69ec6bc5fe767e3337cbe6e0206d84852f6cf
117503445 <t117503445@gmail.com>
2024-11-04 21:23:30 +0000

    wip
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- src/d/main.go
+++ src/d/main.go
@@ -13,28 +13,30 @@ func main() {
 	goutils.InitZeroLog()
 	goutils.ExecOpt.DumpOutput = true
 
-	log.Debug().Msg("Build")
-	goutils.Exec("docker build -t 117503445/eventize-dev .")
-
 	rootDir, err := goutils.FindGitRepoRoot()
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to get git root dir")
 	}
+	dDir := rootDir + "/src/d"   // dev cli dir
+	sDir := rootDir + "/scripts" // scripts dir
+
 	goutils.ExecOpt.Cwd = rootDir
 
+	goutils.Exec("docker build -t 117503445/eventize-dev .", goutils.WithCwd(dDir))
+
 	// docker run -it --rm -v $PWD:/workspace --entrypoint fish 117503445/eventize-dev
-	goutils.Exec(fmt.Sprintf("docker run --rm -v %v:/workspace --entrypoint /workspace/src/dev/build_proto.sh 117503445/eventize-dev", rootDir))
+	goutils.Exec(fmt.Sprintf("docker run --rm -v %v:/workspace --entrypoint /workspace/src/d/build_proto.sh 117503445/eventize-dev", rootDir))
 
-	goutils.Exec("cp src/common/service.pb.ts src/fe/src/rpc/service.pb.ts")
+	goutils.Exec("cp src/common/service.pb.ts src/fe/src/rpc/service.pb.ts", goutils.WithCwd(rootDir))
 
-	goutils.Exec("docker compose exec --no-TTY fe-dev pnpm build")
+	goutils.Exec("docker compose exec --no-TTY fe-dev pnpm build", goutils.WithCwd(sDir))
 
 	// - `-a`：归档模式，表示递归复制文件，并保留文件的权限、时间戳等属性。
 	// - `-v`：详细模式，显示同步过程中的信息。
 	// - `--delete`：删除目标目录（B）中不存在的源目录（A）中的文件。
 	// - `--progress`：在传输过程中显示进度信息。
 	// - `--stats`：在传输结束后显示传输统计信息。
-	goutils.Exec(fmt.Sprintf("rsync -av --delete --progress --stats %v/ %v/", "./src/fe/dist", "./src/be/cmd/server/dist"))
+	goutils.Exec(fmt.Sprintf("rsync -av --delete --progress --stats %v/ %v/", "./src/fe/dist", "./src/be/cmd/server/dist"), goutils.WithCwd(rootDir))
 
 	// 最新 commit
 	r, _ := goutils.Exec("git rev-parse HEAD")
@@ -83,6 +85,6 @@ func main() {
 		log.Fatal().Err(err).Msg("failed to write build_info.json")
 	}
 
-	goutils.Exec("docker compose exec --no-TTY eventize-dev go build ./cmd/server")
-	goutils.Exec("docker compose exec --no-TTY eventize-dev go build ./cmd/agent")
+	goutils.Exec("docker compose exec --no-TTY eventize-dev go build ./cmd/server", goutils.WithCwd(sDir))
+	goutils.Exec("docker compose exec --no-TTY eventize-dev go build ./cmd/agent", goutils.WithCwd(sDir))
 }
